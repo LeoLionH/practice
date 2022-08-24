65e9b056fc075b285a8f7ea31fffdbb0fdf6b3ec
Leo Hardy <leo_hardy@hotmail.co.uk>
2022-08-24 21:03:54 +0100

    Changes to support CMS for blogs

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports= function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addPassthroughCopy("css")
+    return {
+        passthroughFileCopy: true
+  }
 }
