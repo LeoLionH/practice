41636fa93ad08257a27d203157ae1e2ea22edb49
Leo Hardy <leo_hardy@hotmail.co.uk>
2022-09-24 15:11:44 +0100

    Add image and styling for CV

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -4,9 +4,20 @@ const navMenu = document.querySelector(".nav-nested");
 hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    if(hamburger.classList.contains("active")) {
+        navMenu.style.height = "87px";
+        }
+    else navMenu.style.height = "0px"
 });
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+
+if(!navMenu.classList.contains("active")) {
+    navMenu.style.height = "0px";
+    }
+
+
