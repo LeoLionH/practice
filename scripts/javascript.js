03fa077a00f85124348207a79c221cd2a2c28b92
LeoLionH <60005977+LeoLionH@users.noreply.github.com>
2022-10-01 14:48:14 +0100

    Added accessibility elements
    Added accessibility elements

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,13 +1,18 @@
-const hamburger = document.querySelector(".nav-hamburger");
+const hamburger = document.querySelector(".icon");
 const navMenu = document.querySelector(".nav-nested");
+var button = document.querySelector(".icon");
 
 hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
     if(hamburger.classList.contains("active")) {
         navMenu.style.height = "84px";
+        button.setAttribute("aria-expanded", true);
         }
-    else navMenu.style.height = "0px"
+    else {
+        navMenu.style.height = "0px";
+        button.setAttribute("aria-expanded", false)
+    }
 });
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
@@ -18,6 +23,7 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
 
 if(!navMenu.classList.contains("active")) {
     navMenu.style.height = "0px";
+    button == false
     }
 
 
