b2b854478943ac64bf878b6843f0711ba4670238
Leo Hardy <leo_hardy@hotmail.co.uk>
2022-12-17 13:10:16 +0000

    Changed nav to absolute over site content

--- _site/scripts/javascript.js
+++ _site/scripts/javascript.js
@@ -1,17 +1,19 @@
 const hamburger = document.querySelector(".icon");
 const navMenu = document.querySelector(".nav-nested");
+const navItem = document.querySelector(".nav-item");
 var button = document.querySelector(".icon");
 
-hamburger.addEventListener("click", function() {
+hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-    if(hamburger.classList.contains("active")) {
-        navMenu.style.height = "84px";
+    if (hamburger.classList.contains("active")) {
+        navItem.style.height = "100%";
         button.setAttribute("aria-expanded", true);
-        }
-    else {
-        navMenu.style.height = "0px";
-        button.setAttribute("aria-expanded", false)
+        navMenu.style.display = "block";
+    }
+    if (!hamburger.classList.contains("active")) {
+        button.setAttribute("aria-expanded", false);
+        navMenu.style.display = "none";
     }
 });
 
@@ -21,9 +23,12 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
 }))
 
 
-if(!navMenu.classList.contains("active")) {
-    navMenu.style.height = "0px";
+if (!navMenu.classList.contains("active")) {
+    navItem.style.height = "0px";
     button == false
-    }
+}
+
+
+
 
 
